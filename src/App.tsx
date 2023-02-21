6a577d049b2f81da7d8c5b5a19feaf384b11b8e0
Kyrylo Tymchyshyn <kaeser418@gmail.com>
2023-02-21 11:11:00 +0200

    Initialized Project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+import "./css/styles.css";
+import Main from "./pages/main";
+
+function App() {
+    return (
+        <>
+            <Header />
+            <main>
+                <Main />
+            </main>
+        </>
+    );
+}
+
+export default App;
